c73be7423960ef676ff9c73ac261eb2dc99050cb
Leonardo Pessoa <leonardo79pessoa@gmail.com>
2020-09-03 22:41:31 -0300

    .

--- js/menu-scroll.js
+++ js/menu-scroll.js
@@ -1,24 +1,11 @@
 $(function(){
 
-	$('.desktop-menu a').click(function(){
+	$('nav a').click(function(){
 		var href = $(this).attr('href');
 		var offSetTop = $(href).offset().top;
 
 
 		$('html,body').animate({'scrollTop':offSetTop});
-		$('.desktop-menu a').css('color','white');
-		$(this).css('color','black');
-		return false;
-	});
-
-	$('.mobile-menu a').click(function(){
-		var href = $(this).attr('href');
-		var offSetTop = $(href).offset().top;
-
-
-		$('html,body').animate({'scrollTop':offSetTop});
-		$('.mobile-menu a').css('color','#575757');
-		$(this).css('color','black');
 		return false;
 	});
 
